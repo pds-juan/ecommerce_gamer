af335501aca0e4b56ae910833aa3bef975b94fbf
Juan Pires <juan02pires@gmail.com>
2024-10-05 22:27:59 -0300

    cria o backend da aplicação

---
+++ backend/src/produto/produto.controller.ts
@@ -0,0 +1,21 @@
+import { Controller, Get } from '@nestjs/common';
+import { Produto, produtos } from 'src/core';
+
+@Controller('produtos')
+export class ProdutoController {
+  @Get()
+  async obterProdutos(): Promise<Produto[]> {
+    await this.esperarSegundos(2);
+    // return produtos;
+    return produtos.map((produto) => ({
+      ...produto,
+      especificacoes: { destaque: produto.especificacoes.destaque },
+    }));
+  }
+
+  esperarSegundos(segundos: number): Promise<void> {
+    return new Promise((resolve) => {
+      setTimeout(resolve, segundos * 1000);
+    });
+  }
+}
